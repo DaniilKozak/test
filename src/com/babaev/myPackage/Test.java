51b8679007e56862c21521669ea8a3844050d561
Daniil <DDCBrazl@yandex.ru>
2026-02-01 21:12:05 +0500

    The method printList was added

--- src/com/babaev/myPackage/Test.java
+++ src/com/babaev/myPackage/Test.java
@@ -1,7 +1,14 @@
 package com.babaev.myPackage;
 
+import java.util.List;
+
 public class Test {
     public static void main(String... args){
         System.out.print("Hello World");
+    }
+
+
+    public static void printList(List<?> list){
+        System.out.println(list);
     }
 }
